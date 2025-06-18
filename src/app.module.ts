15f4e1cf0b442a1f0500771f85fc10f7631cf77d
Erfan <erfanajoglu098@gmail.com>
2025-06-18 14:53:21 +0330

    Add validation for configModule with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
 import { UsersModule } from './users/users.module';
@@ -7,7 +8,16 @@ import { BlogsModule } from './blogs/blogs.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
+    }),
     TypeOrmModule.forRoot(databaseConfig),
     UsersModule,
     BlogsModule,
